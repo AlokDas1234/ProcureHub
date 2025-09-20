906fb30affd8ca4d67d306da1357c39360228a2f
Alok Das <contact.alokdas@gmail.com>
2025-09-20 13:23:44 +0530

    fixed fonts

--- myapp/consumers.py
+++ myapp/consumers.py
@@ -73,13 +73,24 @@ class ChatConsumer(AsyncWebsocketConsumer):
                 bid_data = await self.get_all_bid_data(dec_val_vi)
                 for item in bid_data:
 
-                    await self.send(text_data=json.dumps({
-                        'type': 'bids_per_requirement',
-                                'bid_id': item['bid_id'],
-                                'bids_by': item['bid_by'],
-                                'requirement': item['requirement'],
-                                'bid_rate': item['bid_rate']
-                    }))
+                    await self.channel_layer.group_send(
+                        self.room_group_name,
+                        {
+                            "type": "bids_per_requirement",
+                            'bid_id': item['bid_id'],
+                            'bids_by': item['bid_by'],
+                            'requirement': item['requirement'],
+                            'bid_rate': item['bid_rate']
+                        }
+                    )
+
+                    # await self.send(text_data=json.dumps({
+                    #     'type': 'bids_per_requirement',
+                    #             'bid_id': item['bid_id'],
+                    #             'bids_by': item['bid_by'],
+                    #             'requirement': item['requirement'],
+                    #             'bid_rate': item['bid_rate']
+                    # }))
 
 
 
